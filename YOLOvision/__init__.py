bfaa7789f0aefbab9ea10da104eb8d7683fdf883
erfanzar <erfanzare82@yahoo.com>
2023-03-30 17:07:00 +0300

    fixing `bug` number `#1`

--- YOLOvision/__init__.py
+++ YOLOvision/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '8.0.58'
+__version__ = '0.0.2'
 
 from YOLOvision.yolo.core.model import YOLO
 from YOLOvision.yolo.utils.checks import check_yolo as checks
@@ -7,6 +7,22 @@ from YOLOvision.yolo.utils.checks import check_yolo as checks
 def prepare_segmentation(model: YOLO):
     model.task = 'segmentation'
     model.overrides['task'] = 'segmentation'
+    model.overrides['cfg_ae'] = 'segmentation-/r'
+    model.overrides['device_local'] = 'auto'
+    try:
+
+        import accelerate
+        model.overrides['init_model_list'] = [accelerate.init_on_device, accelerate.init_empty_weights]
+        model.has_accelerate = True
+        model.accelerate_version = accelerate.__version__
+
+    except ModuleNotFoundError as er:
+
+        model.overrides['init_model_list'] = None
+        model.has_accelerate = False
+        model.accelerate_version = 'v0.0.0.0'
+
+    model.use_bnb = False
     return model
 
 
